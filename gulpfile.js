9bac4ff70954ea934d84ce44816ff2f33432afb7
Raymond <iray100200@hotmail.com>
2024-11-23 14:40:00 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const rename = require('gulp-rename');
 const cssmin = require('gulp-cssmin');
 
 async function buildLess(cb) {
-  return gulp.src('less/*.less')
+  return gulp.src('less/[^_]*.less')
     .pipe(less())
     .pipe(cssmin())
     .pipe(rename({ suffix: '.min' }))
